2eb11f753650ceb7db73bf08d0fc43a17841e1be
RajKumar007test <81877449+RajKumar007test@users.noreply.github.com>
2021-04-04 11:10:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,9 +11,10 @@ worksheet1 = sh.sheet1
 count = 1
 while True:
 	date_time = dt.datetime.now()
-	if date_time.second == 0 and ((date_time.minute % 10) == 0):
+	if date_time.second == 0:
+		print(date_time)
 		cell_add = 'A'+str(count)
 		count = count + 1
 		worksheet1.update(cell_add,date_time.strftime('%d-%m-%Y %H:%M:%S'))
-		time.sleep(570)
+		time.sleep(50)
 
